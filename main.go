39b799d6a130eda911edb412d1328cbd4de5c923
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-10-23 21:30:08 +0300

    Add epsilon flag

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 )
 
 func main() {
+	epsilon := flag.Float64("e", 0.000001, "epsilon")
+	flag.Parse()
+
 	a := 1.0
 	b := 1.0 / math.Sqrt(2)
 	t := 1.0 / 4.0
 	p := 1.0
 	n := 0
-	for math.Abs(a-b) > 0.000001 {
+	for math.Abs(a-b) > *epsilon {
 		aNext := (a + b) / 2.0
 		bNext := math.Sqrt(a * b)
 		tNext := t - p*math.Pow(a-aNext, 2.0)
